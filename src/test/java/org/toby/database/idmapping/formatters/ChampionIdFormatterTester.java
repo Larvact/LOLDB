50637d1939ba49249e69e190875ef5a077e21be8
Tobias Goddard <tobyg7794@hotmail.com>
2021-05-21 14:32:57 +0100

    Added the remaining tests for the championId formatter

--- src/test/java/org/toby/database/idmapping/formatters/ChampionIdFormatterTester.java
+++ src/test/java/org/toby/database/idmapping/formatters/ChampionIdFormatterTester.java
@@ -44,8 +44,104 @@ public class ChampionIdFormatterTester {
 
     @Test
     public void ensureThatTheChampionIdForT1_champ1idHasBeenCorrectlyChanged(){
-        Assert.assertEquals(126, csvReader.getRowDetailsMap().get(2).getT1_champ1id());
-
+        Assert.assertEquals(20, csvReader.getRowDetailsMap().get(24707).getT1_champ1id());
     }
+
+    @Test
+    public void ensureThatTheChampionIdForT1_champ2idHasBeenCorrectlyChanged(){
+        Assert.assertEquals(74, csvReader.getRowDetailsMap().get(36609).getT1_champ2id());
+    }
+
+    @Test
+    public void ensureThatTheChampionIdForT1_champ3idHasBeenCorrectlyChanged(){
+        Assert.assertEquals(12, csvReader.getRowDetailsMap().get(9310).getT1_champ3id());
+    }
+
+    @Test
+    public void ensureThatTheChampionIdForT1_champ4idHasBeenCorrectlyChanged(){
+        Assert.assertEquals(10, csvReader.getRowDetailsMap().get(9416).getT1_champ4id());
+    }
+
+    @Test
+    public void ensureThatTheChampionIdForT1_champ5idHasBeenCorrectlyChanged(){
+        Assert.assertEquals(133, csvReader.getRowDetailsMap().get(22073).getT1_champ5id());
+    }
+
+    @Test
+    public void ensureThatTheChampionIdForT1_ban1HasBeenCorrectlyChanged(){
+        Assert.assertEquals(132, csvReader.getRowDetailsMap().get(35573).getT1_ban1());
+    }
+
+    @Test
+    public void ensureThatTheChampionIdForT1_ban2HasBeenCorrectlyChanged(){
+        Assert.assertEquals(54, csvReader.getRowDetailsMap().get(5499).getT1_ban2());
+    }
+
+    @Test
+    public void ensureThatTheChampionIdForT1_ban3HasBeenCorrectlyChanged(){
+        Assert.assertEquals(12, csvReader.getRowDetailsMap().get(11251).getT1_ban3());
+    }
+
+    @Test
+    public void ensureThatTheChampionIdForT1_ban4HasBeenCorrectlyChanged(){
+        Assert.assertEquals(113, csvReader.getRowDetailsMap().get(19975).getT1_ban4());
+    }
+
+    @Test
+    public void ensureThatTheChampionIdForT1_ban5HasBeenCorrectlyChanged(){
+        Assert.assertEquals(121, csvReader.getRowDetailsMap().get(8359).getT1_ban5());
+    }
+
+    @Test
+    public void ensureThatTheChampionIdForT2_champ1idHasBeenCorrectlyChanged(){
+        Assert.assertEquals(136, csvReader.getRowDetailsMap().get(47209).getT2_champ1id());
+    }
+
+    @Test
+    public void ensureThatTheChampionIdForT2_champ2idHasBeenCorrectlyChanged(){
+        Assert.assertEquals(12, csvReader.getRowDetailsMap().get(33984).getT2_champ2id());
+    }
+
+    @Test
+    public void ensureThatTheChampionIdForT2_champ3idHasBeenCorrectlyChanged(){
+        Assert.assertEquals(123, csvReader.getRowDetailsMap().get(17710).getT2_champ3id());
+    }
+
+    @Test
+    public void ensureThatTheChampionIdForT2_champ4idHasBeenCorrectlyChanged(){
+        Assert.assertEquals(7, csvReader.getRowDetailsMap().get(14525).getT2_champ4id());
+    }
+
+    @Test
+    public void ensureThatTheChampionIdForT2_champ5idHasBeenCorrectlyChanged(){
+        Assert.assertEquals(47, csvReader.getRowDetailsMap().get(4442).getT2_champ5id());
+    }
+
+    @Test
+    public void ensureThatTheChampionIdForT2_ban1HasBeenCorrectlyChanged(){
+        Assert.assertEquals(4, csvReader.getRowDetailsMap().get(24370).getT2_ban1());
+    }
+
+    @Test
+    public void ensureThatTheChampionIdForT2_ban2HasBeenCorrectlyChanged(){
+        Assert.assertEquals(128, csvReader.getRowDetailsMap().get(9237).getT2_ban2());
+    }
+
+    @Test
+    public void ensureThatTheChampionIdForT2_ban3HasBeenCorrectlyChanged(){
+        Assert.assertEquals(12, csvReader.getRowDetailsMap().get(17195).getT2_ban3());
+    }
+
+    @Test
+    public void ensureThatTheChampionIdForT2_ban4HasBeenCorrectlyChanged(){
+        Assert.assertEquals(117, csvReader.getRowDetailsMap().get(28201).getT2_ban4());
+    }
+
+    @Test
+    public void ensureThatTheChampionIdForT2_ban5HasBeenCorrectlyChanged(){
+        Assert.assertEquals(115, csvReader.getRowDetailsMap().get(1885).getT2_ban5());
+    }
+
+
 
 }
