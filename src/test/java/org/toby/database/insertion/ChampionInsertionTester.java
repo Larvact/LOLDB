0a33580edfa2c45753cdad23cbcb3b9a97e4cfab
Tobias Goddard <tobyg7794@hotmail.com>
2021-05-25 16:55:39 +0100

    Added some more stored procedures integrated tests. It might be worth looking at how we are closing the db connections. With these tests there are a lot of opening and closing oif connections which can be quite expensive. Would be better to have one connection overall then close at the end of the test class

--- src/test/java/org/toby/database/insertion/ChampionInsertionTester.java
+++ src/test/java/org/toby/database/insertion/ChampionInsertionTester.java
@@ -16,9 +16,10 @@ import org.toby.reader.LolJsonReader;
 import org.toby.reader.Reader;
 
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class ChampionInsertionTester {
@@ -31,10 +32,11 @@ public class ChampionInsertionTester {
     private static Deletion deletion;
     private LocalDateTime testInitialiserTime;
     private final int expectedNumberOfChampions = 139;
+    private List<String> championList;
     private TestTableDataRetriever testOutcomeRetrieval;
 
     @BeforeClass
-    public static void insertData(){
+    public static void setup(){
         reader = new LolJsonReader(PropertyRetriever.getProperty(PropertyKeys.CHAMPION_DATA_FILE_LOCATION.toString()));
         connector = new LolDbConnector(PropertyRetriever.getProperty(PropertyKeys.DATABASE_CONNECTION_STRING.toString()));
         mapper = new ChampionCollectionMapper(reader);
@@ -69,8 +71,176 @@ public class ChampionInsertionTester {
         Assert.assertEquals(1, testOutcomeRetrieval.getLastestTestResult());
     }
 
+    @Test
+    public void ensureThatAllSpecificChampionsHaveBeenInsertedIntoTheChampionTable(){
+        setupChampionlist();
+        for(String champion : championList) {
+            testOutcomeRetrieval.setTestInitialiserTime(LocalDateTime.now());
+            connector.connect();
+            try (PreparedStatement executeSpSpecificChampionTest = connector.getConnection().prepareStatement("EXECUTE [test].[spSpecificChampionTest] ?")) {
+                executeSpSpecificChampionTest.setString(1, champion);
+                executeSpSpecificChampionTest.execute();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            } finally {
+                try {
+                    connector.getConnection().close();
+                } catch (SQLException throwables) {
+                    throwables.printStackTrace();
+                }
+            }
+            Assert.assertEquals(1, testOutcomeRetrieval.getLastestTestResult());
+        }
+    }
+
     @AfterClass
     public static void deleteChampionData(){
         sqlManager.delete();
+    }
+
+    private void setupChampionlist(){
+        this.championList = new ArrayList<>();
+        championList.add("Aatrox");
+        championList.add("Ahri");
+        championList.add("Akali");
+        championList.add("Alistar");
+        championList.add("Amumu");
+        championList.add("Anivia");
+        championList.add("Annie");
+        championList.add("Ashe");
+        championList.add("Aurelion Sol");
+        championList.add("Azir");
+        championList.add("Bard");
+        championList.add("Blitzcrank");
+        championList.add("Brand");
+        championList.add("Braum");
+        championList.add("Caitlyn");
+        championList.add("Camille");
+        championList.add("Cassiopeia");
+        championList.add("Chogath");
+        championList.add("Corki");
+        championList.add("Darius");
+        championList.add("Diana");
+        championList.add("Dr. Mundo");
+        championList.add("Draven");
+        /*
+        Ekko
+                Elise
+        Evelynn
+                Ezreal
+        Fiddlesticks
+                Fiora
+        Fizz
+                Galio
+        Gangplank
+                Garen
+        Gnar
+                Gragas
+        Graves
+                Hecarim
+        Heimerdinger
+                Illaoi
+        Irelia
+                Ivern
+        Janna
+        Jarvan IV
+        Jax
+                Jayce
+        Jhin
+                Jinx
+        Kalista
+                Karma
+        Karthus
+                Kassadin
+        Katarina
+                Kayle
+        Kayn
+                Kennen
+        Khazix
+                Kindred
+        Kled
+                Kogmaw
+        LeBlanc
+        Lee Sin
+        Leona
+                Lissandra
+        Lucian
+                Lulu
+        Lux
+                Malphite
+        Malzahar
+                Maokai
+        Master Yi
+        Miss Fortune
+        Mordekaiser
+                Morgana
+        Nami
+                Nasus
+        Nautilus
+                Nidalee
+        Nocturne
+                None
+        Nunu
+                Olaf
+        Orianna
+                Ornn
+        Pantheon
+                Poppy
+        Quinn
+                Rakan
+        Rammus
+                Reksai
+        Renekton
+                Rengar
+        Riven
+                Rumble
+        Ryze
+                Sejuani
+        Shaco
+                Shen
+        Shyvana
+                Singed
+        Sion
+                Sivir
+        Skarner
+                Sona
+        Soraka
+                Swain
+        Syndra
+        Tahm Kench
+        Taliyah
+                Talon
+        Taric
+                Teemo
+        Thresh
+                Tristana
+        Trundle
+                Tryndamere
+        Twisted Fate
+        Twitch
+                Udyr
+        Urgot
+                Varus
+        Vayne
+                Veigar
+        Velkoz
+                Vi
+        Viktor
+                Vladimir
+        Volibear
+                Warwick
+        Wukong
+                Xayah
+        Xerath
+        Xin Zhao
+        Yasuo
+                Yorick
+        Zac
+                Zed
+        Ziggs
+                Zilean
+        Zyra
+
+         */
     }
 }
